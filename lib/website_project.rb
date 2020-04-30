470804c7e17d5241a619a1dac661468071e3999e
Hunter Sides <hunter.s.1337@gmail.com>
2020-04-30 21:55:03 +0000

    newest build

--- lib/website_project.rb
+++ lib/website_project.rb
@@ -1,5 +1,11 @@
-require "website_project/version"
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
 
+require_relative 'website_project/version'
+require_relative 'website_project/cli'
+require_relative 'website_project/scraper'
+require_relative 'website_project/website'
 module WebsiteProject
   class Error < StandardError; end
   # Your code goes here...
