f6c8e8c1a06c34d0031df830be5d52117bf8d9a8
Hunter Sides <hunter.s.1337@gmail.com>
2020-05-07 21:02:18 +0000

    fixed issue where ruby \r not found, buy deleting the exe file and creating a new one

--- lib/website_project.rb
+++ lib/website_project.rb
@@ -1,11 +1,15 @@
-require 'pry'
-require 'nokogiri'
-require 'open-uri'
-
 require_relative 'website_project/version'
 require_relative 'website_project/cli'
 require_relative 'website_project/scraper'
 require_relative 'website_project/website'
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+
+
+
 module WebsiteProject
   class Error < StandardError; end
   # Your code goes here...
