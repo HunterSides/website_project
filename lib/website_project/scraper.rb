d5b4cde71152c906ca73776146e35c394b9aba8a
Hunter Sides <hunter.s.1337@gmail.com>
2020-07-16 23:06:52 +0000

    update

--- lib/website_project/scraper.rb
+++ lib/website_project/scraper.rb
@@ -8,14 +8,15 @@ class WebsiteProject::Scraper
     
     results.each do |r| 
       name = r.css(".title").text
-    
-      WebsiteProject::Company.new(name)
+      ref = m.attr("value")
+      WebsiteProject::Company.new(name, ref)
     
     end
   end
   
   def self.scrape_company_info(company)
-    site = "https://www.builtinaustin.com/companies/best-places-to-work-austin-2020"
+    site = "https://www.builtinaustin.com/company/#{company.ref}"
+    binding.pry
     page = Nokogiri::HTML(open(site))
     
     results = page.css(".view-best-places-to-work .view-content .views-row") #need to pass company into scrape
